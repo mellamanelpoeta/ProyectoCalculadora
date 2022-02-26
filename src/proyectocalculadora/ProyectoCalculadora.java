76ccc81f1a7249f22222dfc4bc41bf2ac1203496
maudiaz <maudiaz@192.168.15.17>
2022-02-26 11:14:19 -0600

    agregue un hola a todos

--- src/proyectocalculadora/ProyectoCalculadora.java
+++ src/proyectocalculadora/ProyectoCalculadora.java
@@ -11,7 +11,7 @@ public class ProyectoCalculadora {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola a todos");
     }
     
 }
