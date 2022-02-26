bc01fef5783c409a1c86db2083f6a9bb7b3029ed
Carlos Elías Rivera Mercado <carlos.rivera311202@gmail.com>
2022-02-26 12:04:27 -0600

    correccion de la identacion

--- src/proyectocalculadora/Calculadora.java
+++ src/proyectocalculadora/Calculadora.java
@@ -10,29 +10,29 @@ import java.util.Arrays;
  * Feb 25, 2022
  */
 public class Calculadora {
-    
-    private static boolean revisaSintaxis(String entrada){
-        int i=0,tamaño;
-        boolean resp=true;
-        char pos1,pos2;
+
+    private static boolean revisaSintaxis(String entrada) {
+        int i = 0, tamaño;
+        boolean resp = true;
+        char pos1, pos2;
         ArrayList<Character> operadores;
         ArrayList<Character> numeros;
-        
-        
-        operadores=new ArrayList<Character>(Arrays.asList('+','-','*','/','.'));
-        tamaño=entrada.length(); 
-        
-        
-        if(entrada.charAt(0)=='+'  || entrada.charAt(0)=='*' || entrada.charAt(0)=='/'
-                || entrada.charAt(tamaño-1)=='+'|| entrada.charAt(tamaño-1)=='*'|| entrada.charAt(tamaño-1)=='/'
-                ) //revisa si la entrada comienza o termina con algún asterisco
-            resp=false;
-        while(i<tamaño-1 && resp){
-            pos1=entrada.charAt(i);
-            pos2=entrada.charAt(i+1);
-            if(operadores.contains(pos1) && operadores.contains(pos2))
-                if(pos2!='-') 
-                    resp=false;  
+
+
+        operadores = new ArrayList<Character>(Arrays.asList('+', '-', '*', '/', '.'));
+        tamaño = entrada.length();
+
+
+        if (entrada.charAt(0) == '+' || entrada.charAt(0) == '*' || entrada.charAt(0) == '/'
+                || entrada.charAt(tamaño - 1) == '+' || entrada.charAt(tamaño - 1) == '*' || entrada.charAt(tamaño - 1) == '/'
+        ) //revisa si la entrada comienza o termina con algún asterisco
+            resp = false;
+        while (i < tamaño - 1 && resp) {
+            pos1 = entrada.charAt(i);
+            pos2 = entrada.charAt(i + 1);
+            if (operadores.contains(pos1) && operadores.contains(pos2))
+                if (pos2 != '-')
+                    resp = false;
         }
         return resp;
     }
